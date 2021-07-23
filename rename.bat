df16c8e868b3b5dc172bf40f955fb81eebf0cc84
Yuanyang Lu <53098034+ylu12358@users.noreply.github.com>
2021-07-23 12:51:15 -0400

    Motion

--- rename.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
-SET old=1x
-SET new=Steel Plate 1x
-for /f "tokens=*" %%f in ('dir /b') do (
-  SET newname=%%f
-  SET newname=!newname:%old%=%new%!
-  move "%%f" "!newname!"
-)
